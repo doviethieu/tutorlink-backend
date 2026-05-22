16a8e472c17e541694bb5c54a244639341b69d3a
doviethieu <doviethieu1008@gmail.com>
2026-05-22 17:41:23 +0700

    refactor: Move support routes to src

--- routes/supportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supportController = require('../controllers/supportController');
-const { protect } = require('../middlewares/authMiddleware'); // Ép qua chốt chặn check token
-
-// 1. Cổng gửi khiếu nại/tố cáo (Bắt buộc phải đăng nhập mới được xài - Khớp TC-STU-025)
-router.post('/support', protect, supportController.createSupportTicket);
-
-module.exports = router;
