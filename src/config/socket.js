f230e6f21dddda92e08fdf9c4104623b5e533f90
phungxuandat <phungxuandat003@gmail.com>
2026-05-25 07:40:08 +0700

    chore: update Socket.io real-time configurations

--- src/config/socket.js
+++ src/config/socket.js
@@ -4,26 +4,43 @@ const env = require('./env');
 function configureSocket(server, app) {
   const io = new Server(server, {
     cors: {
-      origin: env.allowedOrigins,
+      origin: env.corsOrigins,
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     },
   });
 
+  app.set('io', io);
   app.set('socketio', io);
 
   io.on('connection', (socket) => {
+    socket.on('join', (roomId) => {
+      if (roomId) socket.join(String(roomId));
+    });
+
     socket.on('join_room', (roomId) => {
-      if (roomId) socket.join(roomId.toString());
+      if (roomId) socket.join(String(roomId));
     });
 
     socket.on('leave_room', (roomId) => {
-      if (roomId) socket.leave(roomId.toString());
+      if (roomId) socket.leave(String(roomId));
+    });
+
+    socket.on('message:send', (payload) => {
+      if (payload?.roomId) {
+        io.to(String(payload.roomId)).emit('message:new', payload);
+      }
     });
 
     socket.on('send_message', (message) => {
       const roomId = message?.roomId || message?.room;
-      if (roomId) socket.to(roomId.toString()).emit('receive_message', message);
+      if (roomId) socket.to(String(roomId)).emit('receive_message', message);
+    });
+
+    socket.on('call:signal', (payload) => {
+      if (payload?.roomId) {
+        socket.to(String(payload.roomId)).emit('call:signal', payload);
+      }
     });
   });
 
