749b9570e8a27aa37ed58b8f993ddb7f29a458e9
phungxuandat <phungxuandat003@gmail.com>
2026-05-25 07:39:53 +0700

    chore: update database connection configurations

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,12 @@ const env = require('./env');
 
 async function connectDB() {
   mongoose.set('strictQuery', true);
-  console.log('[database] connecting to MongoDB...');
+
   await mongoose.connect(env.mongoUri, {
     autoIndex: env.nodeEnv !== 'production',
     serverSelectionTimeoutMS: 15000,
   });
+
   console.log('[database] connected to MongoDB');
 }
 
